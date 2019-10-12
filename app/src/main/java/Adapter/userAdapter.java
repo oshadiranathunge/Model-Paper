2f7cd9a0242a2d389e956849bf267d0e603992d4
oshadiranathungesliit <radoshadi514@gmail.com>
2019-10-13 05:01:42 +0700

    want to add item list complete all interfaces create database complete user create class

--- app/src/main/java/Adapter/userAdapter.java
+++ app/src/main/java/Adapter/userAdapter.java
@@ -4,4 +4,11 @@ import java.util.ArrayList;
 
 public class userAdapter {
     private ArrayList<AddUser>arrayList;
+
+    public userAdapter(ArrayList<AddUser> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    private class AddUser {
+    }
 }

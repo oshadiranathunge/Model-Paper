e1e2383f70c026505d87e2538db3dae4aa9d7b56
“oshadiranathungesliit” <radoshadi514@gmail.com>
2020-10-01 04:01:19 +0530

    Added an array list

--- app/src/main/java/Adapter/userAdapter.java
+++ app/src/main/java/Adapter/userAdapter.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class userAdapter {
     private ArrayList<AddUser>arrayList;
+    private ArrayList<Users>arrayList;
 
     public userAdapter(ArrayList<AddUser> arrayList) {
         this.arrayList = arrayList;
